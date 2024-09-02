ff966eebc39b1958acb5d49321da1bb61facd3cb
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-09-02 12:11:40 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+  public static void main(String[] args) {
+   int contador = 1;
+    String nome = "Bruno";
+
+    
+   while(contador < 11) {
+     System.out.println("bom dia");
+     contador = contador + 1;
+   }
+    System.out.println(nome);
+  }
+
+}
